6929f30b639c04a5c0463c3637e3bdc930cb2a06
Abdullah Hasan <abdullah.hasan@gmail.com>
2021-05-24 16:26:38 +0400

    Completed sections 3 & 4 covering descriptive dataset by activity and final output data set. Updated variable names in the first two sections of code.

--- run_analysis.R
+++ run_analysis.R
@@ -5,41 +5,41 @@
 
         ## Download data files
         if (!file.exists("./projectdata")) {dir.create("./projectdata")}
-        fileurl<-"https://d396qusza40orc.cloudfront.net/
-        getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+        fileurl<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
         download.file(fileurl, 
                       destfile = "./projectdata/dataset.zip")
         unzip(zipfile = "./projectdata/dataset.zip", 
               exdir="./projectdata")
         
         ## Read data files in search directories into R objects
-        xtrain<-read.table("./projectdata/
-                           UCI HAR Dataset/train/x_train.txt")
-        ytrain<-read.table("./projectdata/
-                           UCI HAR Dataset/train/y_train.txt")
-        subjectrain<-read.table("./projectdata/
-                           UCI HAR Dataset/train/subject_test.txt")
+        xtrain<-
+                read.table("./projectdata/UCI HAR Dataset/train/x_train.txt")
+        ytrain<-
+                read.table("./projectdata/UCI HAR Dataset/train/y_train.txt")
+        subjectrain<-
+                read.table("./projectdata/UCI HAR Dataset/train/subject_train.txt")
         
-        xtest<-read.table("./projectdata/
-                           UCI HAR Dataset/test/x_test.txt")
-        ytest<-read.table("./projectdata/
-                           UCI HAR Dataset/test/y_test.txt")
-        subjecttest<-read.table("./projectdata/
-                           UCI HAR Dataset/test/subject_test.txt")
+        xtest<-
+                read.table("./projectdata/UCI HAR Dataset/test/x_test.txt")
+        ytest<-
+                read.table("./projectdata/UCI HAR Dataset/test/y_test.txt")
+        subjecttest<-
+                read.table("./projectdata/UCI HAR Dataset/test/subject_test.txt")
         
-        features<- read.table("./projectdata/
-                           UCI HAR Dataset/features.txt")
-        activities<- read.table("./projectdata/
-                           UCI HAR Dataset/activity_labels.txt")
+        features<- 
+                read.table("./projectdata/UCI HAR Dataset/features.txt")
+        activities<- 
+                read.table("./projectdata/UCI HAR Dataset/activity_labels.txt")
         
-        ## Combine all data sets and name columns
+        ## Combine all data sets and name columns with appropriate 
+        ## descriptive variables
         testdata<-cbind(subjecttest,ytest,xtest)
         names(testdata)<-c("subject_id","activity_id",features[,2])
-        testdata$group<-"test"
+        testdata$group<-1
         
         traindata<-cbind(subjecttrain,ytrain,xtrain)
         names(traindata)<-c("subject_id","activity_id",features[,2])
-        traindata$group<-"train"
+        traindata$group<-2
         
         alldata<-rbind(testdata,traindata)
         
@@ -49,19 +49,25 @@
         reqheaders <- grepl("*-mean*|*-std*", 
                             names(alldata))
         oldheaders <- data.frame(names(alldata))
-        newheaders <- c("subject_id", "activity_id", 
+        newheaders <- c("subject_id", "activity_id","group", 
                         oldheaders[reqheaders==T,])
         
-        newdata <- select(alldata, newheaders)
+        data.mean.std <- select(alldata, newheaders)
         
 ## Use descriptive activity names to name the activities 
-## in the data set
-
-
-## Appropriately label the data set with descriptive variable
-## names. 
-
+## in the data set and sort by subject and activity
+        data.byactivity<- merge(data.mean.std, activities, by.y = "activity_id",all =T)%>%
+                select(c(2,1,83,3,4:82))%>%
+                arrange (subject_id, activity_id)
 
 ## From the data set in step 4, creates a second, 
 ## independent tidy data set with the average of each 
 ## variable for each activity and each subject.
+        data.aggregate <- aggregate(.~subject_id + activity_id,
+                                       data.mean.std, mean)%>%
+                                arrange(subject_id,activity_id)%>%
+                                merge(activities,by.y="activity_id",
+                                      all=T)
+        
+        
+        
