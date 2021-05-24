cd148eece44a320876f4fa4388f40841426e0b77
Abdullah Hasan <abdullah.hasan@gmail.com>
2021-05-24 09:24:48 +0400

    Changed code to read data directly from download link, unzip and into R objects.

--- run_analysis.R
+++ run_analysis.R
@@ -1,21 +1,30 @@
 
 ## Merge the training and the test sets to create one data set.
 
-        ## Establish search directories
-        dirmain<-getwd()
-        dirdata<-paste(dirmain,"/UCI HAR Dataset",sep="")
-        dirtest<-paste(dirdata,"/test", sep="")
-        dirtrain<-paste(dirdata, "/train",sep="")
-        dirtestsign<-paste(dirtest,"/Inertial Signals", sep="")
-        dirtrainsign<-paste(dirtrain,"/Inertial Signals", sep="")
+        ## Download data files
+        if (!file.exists("./projectdata")) {dir.create("./projectdata")}
+        fileurl<-"https://d396qusza40orc.cloudfront.net/
+        getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+        download.file(fileurl, 
+                      destfile = "./projectdata/dataset.zip")
+        unzip(zipfile = "./projectdata/dataset.zip", 
+              exdir="./projectdata")
         
-        filetest<-list.files(dirtest)
-        filetrain<-list.files(dirtrain)
-        filetestsign<-list.files(dirtestsign)
-        filetrainsign<- list.files (dirtrainsign)
-
         ## Read data files in search directories into R objects
+        xtrain<-read.table("./projectdata/
+                           UCI HAR Dataset/train/x_train.txt")
+        ytrain<-read.table("./projectdata/
+                           UCI HAR Dataset/train/y_train.txt")
+        subjectrain<-read.table("./projectdata/
+                           UCI HAR Dataset/train/subject_test.txt")
         
+        xtest<-read.table("./projectdata/
+                           UCI HAR Dataset/test/x_test.txt")
+        ytest<-read.table("./projectdata/
+                           UCI HAR Dataset/test/y_test.txt")
+        subjecttest<-read.table("./projectdata/
+                           UCI HAR Dataset/test/subject_test.txt")
+  
         
 ## Extract only the measurements on the mean and 
 ## standard deviation for each measurement. 
